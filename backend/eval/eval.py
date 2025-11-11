4b4f8df0d7197fab5dd79f3ac90cbb21488bb9e0
trunghoang2002 <trunghoang04122002@gmail.com>
2025-11-11 03:50:49 +0000

    add eval script & testcases

--- backend/eval/eval.py
+++ backend/eval/eval.py
@@ -1,9 +1,32 @@
 from jiwer import wer, cer
+import re
 
-with open("ja/ground_truth_full.txt", "r") as f:
-    ground_truth = f.read()
-with open("ja/realtime/predict_full.txt", "r") as f:
-    prediction = f.read()
+def eval(ground_truth_file, prediction_file):
 
-print("WER:", wer(ground_truth, prediction))
-print("CER:", cer(ground_truth, prediction))
+    with open(ground_truth_file, "r") as f:
+        ground_truth = f.read()
+    with open(prediction_file, "r") as f:
+        prediction = f.read()
+
+    # normalize text
+    ground_truth = ground_truth.lower()
+    prediction = prediction.lower()
+
+    pattern = r"[,.~!?・、。「」『』（）【】〈〉《》！？〜～…‥―：；※]"
+    ground_truth = re.sub(pattern, "", ground_truth)
+    prediction = re.sub(pattern, "", prediction)
+
+    ground_truth = re.sub(r"\s+", " ", ground_truth).strip()
+    prediction = re.sub(r"\s+", " ", prediction).strip()
+
+    print("WER:", wer(ground_truth, prediction))
+    print("CER:", cer(ground_truth, prediction))
+
+lang = "en" # ja | en
+type = "upload" # realtime | upload
+model = "whisper-small" # sensevoice-small | whisper-small | whisper-medium
+
+ground_truth_file = f"{lang}/ground_truth_full.txt"
+prediction_file = f"{lang}/{type}/{model}/predict_full.txt"
+
+eval(ground_truth_file, prediction_file)
